af6bc6d8f462cb2fb8a89f473bc5daab75d1237a
“Ronny <contact@ronnyperez.com>
2020-02-12 22:07:08 -0600

    started homework, stock on API part

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
-const questions = [
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const convertFactory = require("electron-html-to");
+const open = require("open");
+// const api = require("./api");
+// const genereateHTML = require("./generateHTML");
 
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'list',
+        name: 'color',
+        message: 'What is your favorite color?',
+        choices: ['red','blue','green','pink']
+    }
 ];
 
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),
+    fileName), data);
 }
 
 function init() {
+    inquirer.prompt(questions).then(({ github, color }) => {
+        console.log('Searching...');
 
+        // api
+        //     .getUser(github)
+        //     .then(response =>
+        //         api.getTotalStars(github).then(stars => {
+        //             return generateHTML({
+        //                 stars,
+        //                 color,
+        //                 ...response.data
+        //             });
+        //         }))
+        //         .then(html => {
+        //             const conversion = convertFactory({
+        //                 converterPath: convertFactory.converters.PDF
+        //             });
+
+        //             conversion({ html }, function (err, result) {
+        //                 if (err) {
+        //                 }
+
+        //                 result.stream.pipe(
+        //                     fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+        //                 );
+
+        //                 conversion.kill();
+        //             });
+
+        //             open(path.join(process.cwd(), "resume.pdf"));
+        //         });
+    });
 }
-
 init();
